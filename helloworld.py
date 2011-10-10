30b81c149ee3a47d82b763709bbac30c27f1c621
Nick Loadholtes <nick@ironboundsoftware.com>
2011-10-09 21:18:10 -0400

    Updating the name Correcting typos. This is why I don't do coding with vi.

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,6 @@
 
 print 'content-Type: text/html'
 print ''
-print '<html><body><h1>You are probably looking for <a href="http://twitter.com/!#/RedactedTweet/">this</a></h1>`
-print 'Source available from <a href="http://github.com/nloadholtes/Redacted-Tweets">GitHub</a>"
+print '<html><body><h1>You are probably looking for <a href="http://twitter.com/!#/RedactedTweet/">this</a></h1>'
+print 'Source available from <a href="http://github.com/nloadholtes/Redacted-Tweets">GitHub</a>'
 print '</body></html>'
